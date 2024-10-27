1650e376f341a38f10cf69f5be83d8e9f3710fb2
qualutia <a.paul.burghelea@gmail.com>
2024-10-27 21:49:11 +0000

    feat: Update app switch functions for grids

--- js/app.js
+++ js/app.js
@@ -4,12 +4,19 @@
 
 let camera, controls, controls2, scene, scene2, renderer, renderer2;
 
-init();
-animate();
 
-const data = {
+
+const store = {
   gridSize: null,
 
+  geometry: {
+    grid: {},
+    panels: {}
+  },
+
+  current: {
+    size: null,
+  },
 
   sizes: [],
   thicknesses: [],
@@ -17,62 +24,61 @@ const data = {
   heights: []
 }
 
+let panelFilter = {
+  size: null,
+  thickness: null,
+  offset: null,
+  height: null
+}
+
+let currentPanel = 0
 
 //Material used in the scene
 const highlightMaterial = new THREE.MeshStandardMaterial({ color: '#FF2400', metalness: 0.5 })
 const defaultMaterial = new THREE.MeshStandardMaterial({ color: '#e67e22', metalness: 0.5 })
 const wireframeMaterial = new THREE.MeshNormalMaterial({ color: 'white', wireframe: false })
 
+init();
+animate();
+
+
+
+
+
+
 function unique(value, index, array) {
   return array.indexOf(value) === index;
 }
 
+/**
+ * Get all the grid cells for the current size configuration
+ * @returns 
+ */
+function getCurrentGrid() {
+  return scene.children.filter((item) => item.name === 'target' && item.userData.size == store.current.size)
+}
+
+function getAllGrids() {
+  return scene.children.filter((item) => item.name === 'target')
+}
+
+function getPanels() {
+  return scene.children.filter((item) => item.name === 'panel' && item.userData.size == store.current.size)
+}
+
+function getAllPanels() {
+  return scene.children.filter((item) => item.name === 'panel')
+}
+
 function init() {
 
-  scene = new THREE.Scene();
-  container = document.getElementById('container');
-
-  // create the rendered and set it to the height/width of the container
-  renderer = new THREE.WebGLRenderer({ antialias: true });
-  renderer.setSize(container.clientWidth, container.clientHeight);
-  renderer.shadowMap.enabled = true; // if you don't want shadows, set to false
-  container.appendChild(renderer.domElement);
-  renderer.setClearColor(0x000000, 0)
-  renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.shadowMap.enabled = true;
-  renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-
-  camera = new THREE.PerspectiveCamera(60, container.clientWidth / container.clientHeight, 0.5, 800000);
-  camera.position.set(-120000, 120000, -70000); // starting position of the camera
-
-  controls = new THREE.OrbitControls(camera, renderer.domElement);
-  controls.enableDamping = true
-  controls.dampingFactor = 0.1
-  controls.screenSpacePanning = true;
-  controls.maxPolarAngle = Math.PI / 2;
-  controls.maxDistance = 45000;
-  controls.minDistance = 15000;
-  controls.enablePan = false;
+  //Create ThreeJS scenes
+  initMainScene()
+  intiPanelScene()
 
 
-  // Scene 2
-  scene2 = new THREE.Scene();
-  container2 = document.getElementById('container-secondary');
-
-  renderer2 = new THREE.WebGLRenderer({ antialias: true, alpha: true })
-  renderer2.setSize(container2.clientWidth, container2.clientHeight)
-  renderer2.shadowMap.enabled = true
-  renderer2.setClearColor(0x000000, 0)
-  container2.appendChild(renderer2.domElement)
-
-  camera2 = new THREE.PerspectiveCamera(10, container2.clientWidth / container2.clientHeight, 0.5, 800000);
-  camera2.position.set(8000, 8000, 3000); // starting position of the camera
-
-  controls2 = new THREE.OrbitControls(camera2, renderer2.domElement);
-
-  controls2.minPolarAngle = Math.PI / 4;    // Minimum vertical rotation
-  controls2.maxPolarAngle = Math.PI / 4;    // Maximum vertical rotation
-
+  //Initialise the window events for the UI
+  initUIEvents()
 
 
 
@@ -95,24 +101,34 @@ function init() {
       scene2.add(focus)
 
 
-      const panels = scene.children.filter((item)=> item.name == 'panel');
-      const targets = scene.children.filter((item)=> item.name == 'target');
-
-
-      data.heights = panels.map((item)=> item.userData.height).filter(unique).sort((a, b) => a - b)
-      data.offsets = panels.map((item)=> item.userData.offset).filter(unique).sort((a, b) => a - b)
-      data.sizes = panels.map((item)=> item.userData.size).filter(unique).sort((a, b) => a - b)
-      data.thicknesses = panels.map((item)=> item.userData.thickness).filter(unique).sort((a, b) => a - b)
-
-
-      console.log(data)
-      console.log(panels)
+      const panels = scene.children.filter((item) => item.name == 'panel');
+      const targets = scene.children.filter((item) => item.name == 'target');
 
 
 
+      store.heights = panels.map((item) => item.userData.height).filter(unique).sort((a, b) => a - b)
+      store.offsets = panels.map((item) => item.userData.offset).filter(unique).sort((a, b) => a - b)
+      store.sizes = panels.map((item) => item.userData.size).filter(unique).sort((a, b) => a - b)
+      store.thicknesses = panels.map((item) => item.userData.thickness).filter(unique).sort((a, b) => a - b)
 
 
+      store.current.size = store.sizes[0]
 
+
+      // Default panel filter
+      panelFilter = {
+        size: store.sizes[0],
+        thickness: store.thicknesses[0],
+        offset: store.offsets[0],
+        height: store.heights[0]
+      }
+
+
+      initGrid()
+
+      updateGrid()
+
+      // setGrid([0])
 
     },
     function (obj) {
@@ -124,104 +140,6 @@ function init() {
   )
   //#endregion
 
-
-
-
-  let prevSelection = null;
-
-  document.addEventListener('mousedown', onDocumentMouseDown);
-
-
-
-  function onDocumentMouseDown(event) {
-    event.preventDefault();
-
-    if (event.button != 2)
-      return
-
-    let mouse3D = new THREE.Vector3((event.clientX / window.innerWidth) * 2 - 1,
-      -(event.clientY / window.innerHeight) * 2 + 1,
-      0.5);
-
-
-
-
-    let raycaster = new THREE.Raycaster();
-    raycaster.setFromCamera(mouse3D, camera);
-    let intersects = raycaster.intersectObjects(scene.children.filter((object) => object.name === 'target'));
-
-
-    if (intersects.length > 0) {
-
-      if (prevSelection != null)
-        prevSelection.material = defaultMaterial
-
-
-
-      const object = intersects[0].object
-      object.material = highlightMaterial
-
-
-      const index = getGrid().indexOf(object)
-      changeCell(index)
-
-      // object.material.color.setHex('#EE4B2B')
-      // object.material.opacity.setOpacity(1)
-
-      console.log(intersects[0].object.userData)
-      console.log(intersects[0].object)
-
-      prevSelection = object
-      // intersects[0].object.material.color.setHex(Math.random() * 0xffffff);
-    }
-
-
-  }
-
-
-  function getGrid() {
-    const grid = scene.children.filter((item) => item.name === 'target')
-    return grid
-  }
-
-  camera.eulerOrder = "XZY";
-
-  function changeCell(cell) {
-
-    const grid = getGrid()
-    const panel = scene.children.filter((item) => item.name === 'panel')[0].clone()
-
-    const target = grid[cell]
-
-
-    console.log(target.userData.rotation)
-    panel.position.copy(target.position);
-    // panel.rotation.set(new THREE.Vector3(0,0,0))
-    // panel.rotation.copy( target.rotation );
-    // panel.rotation.x = rotation.x
-
-    panel.rotation.x = 1.571
-    panel.rotation.z = 1.571
-    //Quick fix on the rotation on Y
-    // if (rotation.y < 0) {
-
-    //   // panel.rotation.y = 3.14159
-    //   const scale = new THREE.Vector3(1, 1, 1);
-    //   scale.x *= -1;
-    //   panel.scale.multiply(scale)
-    // }
-
-
-
-
-
-
-    scene.add(panel)
-
-    scene.remove(grid[cell])
-  }
-
-  window.addEventListener('resize', onWindowResize, false);
 }
 
 function progressLog(message) {
@@ -246,12 +164,7 @@ function progressLog(message) {
   container.appendChild(progress)
 }
 
-function onWindowResize() {
-  camera.aspect = container.clientWidth / container.clientHeight;
-  camera.updateProjectionMatrix();
 
-  renderer.setSize(container.clientWidth, container.clientHeight);
-}
 
 function animate() {
 
@@ -295,11 +208,283 @@ function render() {
   renderer2.render(scene2, camera2);
 }
 
+/**
+ * Set the current panel in memory
+ * @param {string} id ID of the current panel 
+ */
+function setCurrentPanel(id) {
+
+  panels = getPanels()
+
+  try {
+    currentPanel = id
+
+    const tempPanel = panels[id].clone()
+    tempPanel.material = wireframeMaterial
+
+    scene2.remove(scene2.children.filter((item) => item.name == 'panel')[0])
+
+    scene2.add(tempPanel)
+
+
+  }
+  catch (err) {
+    console.log(`Panel with ID ${id} doesn't exist!`)
+  }
+
+}
+
+
+
 
 function genSliders() {
 
-  
-  
 
 
+
+
+}
+
+
+function initMainScene() {
+  scene = new THREE.Scene();
+  container = document.getElementById('container');
+
+  // create the rendered and set it to the height/width of the container
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(container.clientWidth, container.clientHeight);
+  renderer.shadowMap.enabled = true; // if you don't want shadows, set to false
+  container.appendChild(renderer.domElement);
+  renderer.setClearColor(0x000000, 0)
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+  camera = new THREE.PerspectiveCamera(60, container.clientWidth / container.clientHeight, 0.5, 800000);
+  camera.position.set(-120000, 120000, -70000); // starting position of the camera
+
+  controls = new THREE.OrbitControls(camera, renderer.domElement);
+  controls.enableDamping = true
+  controls.dampingFactor = 0.1
+  controls.screenSpacePanning = true;
+  controls.maxPolarAngle = Math.PI / 2;
+  controls.maxDistance = 45000;
+  controls.minDistance = 15000;
+  controls.enablePan = false;
+}
+
+function intiPanelScene() {
+  // Scene 2
+  scene2 = new THREE.Scene();
+  container2 = document.getElementById('container-secondary');
+
+  renderer2 = new THREE.WebGLRenderer({ antialias: true, alpha: true })
+  renderer2.setSize(container2.clientWidth, container2.clientHeight)
+  renderer2.shadowMap.enabled = true
+  renderer2.setClearColor(0x000000, 0)
+  container2.appendChild(renderer2.domElement)
+
+  camera2 = new THREE.PerspectiveCamera(10, container2.clientWidth / container2.clientHeight, 0.5, 800000);
+  camera2.position.set(8000, 8000, 3000); // starting position of the camera
+
+  controls2 = new THREE.OrbitControls(camera2, renderer2.domElement);
+
+  controls2.minPolarAngle = Math.PI / 4;    // Minimum vertical rotation
+  controls2.maxPolarAngle = Math.PI / 4;    // Maximum vertical rotation
+
+}
+
+function initUIEvents() {
+
+
+  let prevSelection = null;
+
+
+  /**
+   * Track the mouse click function on the placed panels
+   * On click we replace the panel with the current selection from the `Select Panel`
+   */
+  document.addEventListener('mousedown', function (e) {
+    if (e.button != 2)
+      return
+
+    let mouse3D = new THREE.Vector3((e.clientX / window.innerWidth) * 2 - 1, -(e.clientY / window.innerHeight) * 2 + 1, 0.5);
+
+
+    let raycaster = new THREE.Raycaster();
+    raycaster.setFromCamera(mouse3D, camera);
+    let intersects = raycaster.intersectObjects(scene.children.filter((object) => object.name === 'target'));
+
+
+    if (intersects.length > 0) {
+
+      if (prevSelection != null)
+        prevSelection.material = defaultMaterial
+
+      const object = intersects[0].object
+      object.material = highlightMaterial
+
+
+      const index = getCurrentGrid().indexOf(object)
+      changeCell(index)
+
+      // console.log(intersects[0].object.userData)
+      // console.log(intersects[0].object)
+
+      prevSelection = object
+    }
+  });
+
+
+
+  /**
+   * Update the camera and render functions after the browser window is resized
+   */
+  window.addEventListener('resize', function (e) {
+    camera.aspect = container.clientWidth / container.clientHeight
+    camera.updateProjectionMatrix()
+
+    renderer.setSize(container.clientWidth, container.clientHeight)
+
+  }, false)
+
+
+  document.getElementById("set-form").addEventListener("submit", function (e) {
+    e.preventDefault()
+
+    const values = e.target.elements['set'].value
+
+    try {
+      setGrid(JSON.parse(values))
+    }
+    catch (err) {
+      console.log(err)
+    }
+  })
+
+  document.getElementById("grid-select").addEventListener("change", function (e) {
+    store.current.size = e.target.value
+    updateGrid()
+  })
+
+
+
+  document.getElementById("button-option-next").addEventListener("click", function () {
+    if (currentPanel < getPanels().length) {
+      setCurrentPanel(currentPanel + 1)
+    }
+    else {
+      setCurrentPanel(0)
+    }
+  })
+
+
+  document.getElementById("button-option-prev").addEventListener("click", function () {
+    if (currentPanel - 1 < 0) {
+      setCurrentPanel(getPanels().length - 1)
+    }
+    else {
+      setCurrentPanel(currentPanel - 1)
+    }
+  })
+
+
+
+}
+
+
+/**
+ * 
+ * @param {number} cell Cell from the grid to replace
+ * @param {number} replace ID of the panel to replace the cell, defaults to the current panel
+ */
+function changeCell(cell, replace = currentPanel) {
+
+  const grid = getCurrentGrid()
+  const panels = getPanels()
+  const panel = store.geometry.panels[store.current.size][0][replace].clone()
+
+  const target = grid[cell]
+
+  panel.position.copy(target.position);
+
+  panel.rotation.x = 1.571
+  panel.rotation.z = 1.571
+
+
+  scene.add(panel)
+
+  scene.remove(grid[cell])
+}
+
+
+function initGrid() {
+
+  const allGrids = getAllGrids()
+  const allPanels = scene.children.filter((item) => item.name === 'panel')
+
+
+
+  store.sizes.forEach((item) => {
+    store.geometry.panels[item] = [
+      allPanels.filter((cell) => cell.userData.size == item)
+    ]
+  })
+
+  store.sizes.forEach((item) => {
+    store.geometry.grid[item] = [
+      allGrids.filter((cell) => cell.userData.size == item)
+    ]
+  })
+
+  // Remove all grid cells
+  allGrids.forEach((cell) => {
+    scene.remove(cell)
+  });
+
+}
+
+
+function updateGrid() {
+
+  const allGrids = getAllGrids()
+  const allPanels = scene.children.filter((item) => item.name === 'panel')
+  // Remove all grid cells
+  allGrids.forEach((cell) => {
+    scene.remove(cell)
+  });
+
+  allPanels.forEach((panel) => {
+    scene.remove(panel)
+  });
+
+  store.geometry.grid[store.current.size][0].forEach((cell) => {
+
+    cell.scale.x = 0.5
+
+    scene.add(cell)
+  })
+
+}
+
+
+function setGrid(values) {
+
+
+  if (values.length == 1) {
+
+    const grid = getCurrentGrid()
+    console.log(`Setting grid for ${grid.length} cells`)
+
+    for (let i = 0; i < grid.length; i++) {
+      changeCell(i, values[0])
+    }
+  }
+  else {
+    console.log(`Setting grid for ${values.length} cells`)
+
+    for (let i = 0; i < values.length; i++) {
+      changeCell(i, values[i])
+    }
+  }
 }
